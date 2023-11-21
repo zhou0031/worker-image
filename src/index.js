9b27e367f1c38569cd4aaa5df5bfdce4447a95cb
ryan <ryan.z.williams@gmail.com>
2023-11-21 12:02:26 +0800

    disables somethoing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 export default {
 	async fetch(request, env, ctx) {
+		/*
+		const url = new URL(request.url);
+		const searchParams = url.searchParams;
+		const accessToken = searchParams.get('access_token');
+		
+		if(!accessToken){
+			return new Response("Token Missing",{
+				status:401
+			})
+		}
+		
+		const isValid=totp.check(accessToken,env.SECRET)
+		if(!isValid)
+			return new Response("Invalid Token: "+accessToken,{
+			status:401
+		})
+		*/
 		return await handleRequest(request);
 	},
 };
 
 
 async function handleRequest(request){
-	const requestUrl = new URL(request.url);
-	const TILE_URL = 'https://tile.openstreetmap.org'+requestUrl.pathname
-	
+	const imageUrlParam = new URL(request.url).searchParams.get('url');
+		
+	// Check if the 'url' parameter is present
+	if (!imageUrlParam) {
+		return new Response('Image URL not provided', {
+		  status: 400,
+		  headers: { 'Content-Type': 'text/plain' },
+		});
+	}
+
 	const requestHeaders = {
 		'User-Agent': 'Your-Application-User-Agent',
 		'Referer': 'https://310soft.com',
@@ -22,7 +46,7 @@ async function handleRequest(request){
 	  };
 
 	  try {
-		const response = await fetch(TILE_URL, requestInit);
+		const response = await fetch(imageUrlParam, requestInit);
 	
 		// Check if the response status is OK (200)
 		if (response.ok) {
